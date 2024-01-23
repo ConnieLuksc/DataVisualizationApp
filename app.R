b0008b2e5e5856574b5b207126b2101c86ff161a
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2024-01-23 17:17:36 -0600

    Show umap

--- app.R
+++ app.R
@@ -1,51 +1,167 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
 
-library(shiny)
+source('global.R')
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+ui <- dashboardPage(
+    dashboardHeader(title = "scRNAseq Analysis"),
+    dashboardSidebar(
+        tags$head(
+        tags$style(HTML(".skin-blue .main-header .sidebar-toggle {display: none;}"))
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        sidebarMenu(id='tab',
+            useShinyjs(),
+            menuItem("Home Page", tabName = "home", icon = icon("list")),
+            menuItem("scRNAseq Analyzer", tabName = "input", icon = icon("edit")),
+            conditionalPanel(condition = "input.tab == 'input'",
+                div(
+                    fileInput("file", "Upload File", multiple=TRUE, accept=c('.rds')),
+                    actionButton("reset", "Reset", icon = icon("undo"), style = "color: #fff; background-color: #dc3545; width: 87.25%"),
+                    actionButton("run", "Run", icon = icon("play"), style = "color: #fff; background-color: #28a745; width: 87.25%")
+                    )
+                )
         )
-    )
+    ), 
+    dashboardBody(
+            tabItems(
+                tabItem(tabName = "input", # tabItem refers to tab in sidebar (not main panel)
+                tabsetPanel(id = 'main_tabs',
+                    tabPanel("Instructions",
+                            includeMarkdown("./markdown/instructions.md")
+                            )
+                        )
+                    ),
+                tabItem(tabName = "home",
+                tags$h1(HTML("<u>Welcome to The scRNAseq Suerat analysis RShiny app</u>")),
+                )
+                )
+            )         
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+    options(shiny.maxRequestSize=300*1024^2)
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    values <- reactiveValues()
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    # Disable Run by default
+    shinyjs::disable("run")
+
+    observe({
+    if(is.null(input$file) != TRUE) {
+        shinyjs::enable("run")
+    } else {
+        shinyjs::disable("run")
+    }
     })
+
+    observeEvent(input$run, {
+        shinyjs::disable("run")
+
+        # Clear tabs before 'Run' is ran another time
+        removeTab("main_tabs", "UMAP")
+        removeTab("main_tabs", "Gene Expression")
+
+        show_modal_spinner(text = "Preparing plots...")
+
+        obj <- load_seurat_obj(input$file$datapath)
+        if (is.vector(obj)){
+            showModal(modalDialog(
+                title = "Error with file",
+                HTML("<h5>There is an error with the file you uploaded. See below for more details.</h5><br>",
+                    paste(unlist(obj), collapse = "<br><br>"))
+            ))
+            shinyjs::enable("run")
+
+        } else {
+            
+            output$umap <- renderPlot({
+                if (!is.null(input$metadata_col)) {
+                    create_metadata_UMAP(obj, input$metadata_col)
+                }
+            })
+
+            output$featurePlot <- renderPlot({
+                if (!is.null(input$gene)) {
+                    create_feature_plot(obj, input$gene)
+                }
+            })
+
+            output$downloadFeaturePlot <- downloadHandler(
+                filename = function(){
+                    paste0(input$gene, '_feature_plot', '.png')
+                },
+                content = function(file){
+                    plot <- create_feature_plot(obj, input$gene)
+                    ggsave(filename=file, width = 10, height = 5, type = "cairo")
+                }
+            )
+            output$download_umap <- downloadHandler(
+                filename = function(){
+                    paste0(input$metadata_col, '_UMAP', '.png')
+                },
+                content = function(file){
+                    plot <- create_metadata_UMAP(obj, input$metadata_col)
+                    ggsave(filename=file, width = 10, height = 5, type = "cairo")
+                }
+            )
+
+            insertTab(
+                inputId = "main_tabs",
+                tabPanel(
+                    "UMAP",
+                    fluidRow(
+                    column(
+                        width = 8,
+                        plotOutput(outputId = 'umap'),
+                        downloadButton("download_umap", "Download UMAP")
+                    ),
+                    column(
+                        width = 4,
+                        selectizeInput("metadata_col", 
+                            "Metadata Column", 
+                            colnames(obj@meta.data)
+                        )
+                    )
+                    ),
+                    style = "height: 90%; width: 95%; padding-top: 5%;"
+                ),
+                select = TRUE
+            )
+            insertTab(
+                inputId = "main_tabs",
+                tabPanel(
+                    "Gene Expression",
+                    fluidRow(
+                    column(
+                        width = 8,
+                        plotOutput(outputId = 'featurePlot'),
+                        downloadButton("downloadFeaturePlot", "Download Feature Plot")
+                    ),
+                    column(
+                        width = 4,
+                        selectizeInput("gene", 
+                            "Genes", 
+                            rownames(obj)
+                        )
+                    )
+                    ),
+                    style = "height: 90%; width: 95%; padding-top: 5%;"
+                )
+            )
+
+            remove_modal_spinner()
+            shinyjs::enable("run")
+            
+        }
+    })
+
+    # Clear all sidebar inputs when 'Reset' button is clicked
+    observeEvent(input$reset, {
+        shinyjs::reset("file")
+        removeTab("main_tabs", "UMAP")
+        removeTab("main_tabs", "Gene Expression")
+        shinyjs::disable("run")
+    })
+
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
+
