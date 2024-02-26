6436a6f6a1482d98b8bdd1ce1f340b18a4ded695
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2024-02-25 20:15:23 -0600

    merge

--- global.R
+++ global.R
@@ -102,66 +102,11 @@ create_feature_plot <- function(obj, gene, values) {
 }
 
 create_violin_plot <- function(obj, gene, values, ncol, pt.size) {
-<<<<<<< HEAD
     VP <- NULL
     if (gene %in% rownames(obj)) {
         VP <- VlnPlot(obj, features = gene, pt.size = 0, combine = FALSE)
     }else{
         VP <- ggplot() +
-=======
-  VP <- NULL
-  if (gene %in% rownames(obj)) {
-    VP <- VlnPlot(obj, features = gene, pt.size = 0.001, combine = FALSE)
-  } else {
-    VP <- ggplot() +
-      theme_void() +
-      geom_text(aes(x = 0.5, y = 0.5, label = str_wrap("Gene doesn't exist", width = 20)), size = 12, color = "gray73", fontface = "bold") +
-      theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
-  }
-  values$violin <- VP
-  return(VP)
-}
-
-create_mds_plot <- function(obj, values) {
-  # aggregate
-  all_genes <- AggregateExpression(obj)
-
-  # create a deglist object
-  gene_names <- rownames(obj@assays$RNA)
-  dge_data <- DGEList(counts = all_genes$RNA)
-  dge_data$genes <- gene_names
-
-  # create MDS plot
-  set.seed(123)
-  colors <- rainbow(length(colnames(all_genes$RNA)))
-  # mdsPlot <- plotMDS(dge_data, col = colors)
-  mdsPlot <- plotMDS(dge_data, col = colors, pch=20, cex=2)
-  legend("topright", legend = colnames(all_genes$RNA), col = colors, pch = 20, cex = 0.8, pt.cex = 0.8, title = "Group")
-  title("MDS Plot")
-  values$mds <- mdsPlot
-  return (mdsPlot)
-}
-
-# visualize annotation
-create_annotation_UMAP <- function(obj, col, pc, resolution, values, annotation) {
-  tryCatch(
-  {
-    obj <- FindNeighbors(obj, dims = 1:pc)
-    obj <- FindClusters(obj, resolution = resolution)
-    obj <- RunUMAP(obj, dims = 1:pc)
-    umap <- DimPlot(obj,
-                    pt.size = .1, label = FALSE,
-                    label.size = 4, group.by = "Annotation",
-                    reduction = "umap"
-    )
-    remove_modal_spinner()
-    values$obj <- obj
-    values$umap_annotation <- umap
-    return(umap)
-  },
-    error = function(err) {
-      umap <- ggplot() +
->>>>>>> origin/Ruiyang_Peng
         theme_void() +
         geom_text(
           aes(
@@ -176,13 +121,6 @@ create_annotation_UMAP <- function(obj, col, pc, resolution, values, annotation)
       # remove_modal_spinner()
       return(umap)
     }
-<<<<<<< HEAD
-    values$violin <- VP
-    return(VP)
-}
-
-=======
-  )
 }
 
 # sankey plot
@@ -232,4 +170,3 @@ create_sankey_plot <- function(obj, values, pc, resolution, annotation, cluster_
   return(sankey)
 
 }
->>>>>>> origin/Ruiyang_Peng
